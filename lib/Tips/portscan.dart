7b9643eae5a7fd0d8c90c276fac91b2c1a4c5205
e <yzp@live.cn>
2019-04-12 11:02:38 +0800

    端口扫描：添加了提交错误提示逻辑

--- lib/Tips/portscan.dart
+++ lib/Tips/portscan.dart
@@ -81,13 +81,22 @@ class _PortScanWidgetState extends State<PortScanWidget> {
           onPressed: (){
             var ipStr =  _ipController.text;
             var portStr = _portController.text;
-            
-            if(ipStr!=null){
-              setState(() {
-              _portBind=PortBind.init();
-              // _portBind.ip=ipStr;
-              _portBind.setIp(ipStr);
-              var checkedPorts=_tileList.list;
+            setState(() {
+             _portBind=PortBind.init(); 
+            });
+            //IP文本框不为空时填充对象IP地址
+            if(ipStr!=null&&ipStr!=""){
+              setState(() {           
+              _portBind.setIp(ipStr);  
+            });
+            }
+            // else{
+            //   print("ipStr NULL");
+            //   showToast("请填写IP地址");
+            // }
+            //填充端口
+            var checkedPorts=_tileList.list;
+            setState(() {
               for (var item in checkedPorts) {
                if(item.checked){
                  _portBind.port.add(item.port);
@@ -99,17 +108,20 @@ class _PortScanWidgetState extends State<PortScanWidget> {
                 }  
               }
             });
-            }else{
-              print("ipStr NULL");
-            }
-            if(_portBind.port.length!=0){
+            bool portFlag=_portBind.port.length!=0;
+            bool ipFlag=_portBind.ip!="";              
+            if(portFlag&&ipFlag){
               String s=_portBind.toString();
               print(s);
               ScanPortByJson(s);
-            }else{
-              print("port null");
+            }else if(!portFlag&&!ipFlag){
+             // print("port null");
+              showToast("请填写IP地址并至少添加一个端口号"); 
+            }else if(!portFlag&&ipFlag){
+              showToast("请至少添加一个端口号");
+            }else if(portFlag&&!ipFlag){
+              showToast("请填写IP地址");
             }
-           
           },
         ),
       ],
@@ -163,4 +175,13 @@ class _PortScanWidgetState extends State<PortScanWidget> {
   Future<String> _getTileJson() {
     return rootBundle.loadString("lib/assets/ports.json");
   }
+  //通过平台代码弹出Toast提示信息
+  showToast(String msg) async {
+    try {
+      await platform.invokeMethod("showToast", {"msg": msg});
+    } on PlatformException catch (e) {
+      print("Toast出错:" + e.toString());
+    }
+  }
+
 }
